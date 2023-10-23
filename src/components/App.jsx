28a4b136ab7df6c0acd14624064cacb7a74cb412
SerhiiPochtovyk <sergey.po4tovik@gmail.com>
2023-10-23 16:07:09 +0300

    create feedback;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import React, { useState } from 'react';
+import 'bulma/css/bulma.css';
+// import { nanoid } from 'nanoid';
+
+
 export const App = () => {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const handleFeedback = (feedbackType) => {
+    setState((prevState) => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1
+    }));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Feedback App</h1>
+      <button onClick={() => handleFeedback('good')}>Good</button>
+      <button onClick={() => handleFeedback('neutral')}>Neutral</button>
+      <button onClick={() => handleFeedback('bad')}>Bad</button>
+      <div>
+        Good: {state.good}
+      </div>
+      <div>
+        Neutral: {state.neutral}
+      </div>
+      <div>
+        Bad: {state.bad}
+      </div>
     </div>
   );
 };
